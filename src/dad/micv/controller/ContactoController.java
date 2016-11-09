655db4bc5e0e2c7e9631a3ce1f0d707e673f5761
GabriMagic <champy9a@gmail.com>
2016-11-09 23:19:05 +0000

    asd

--- src/dad/micv/controller/ContactoController.java
+++ src/dad/micv/controller/ContactoController.java
@@ -1,14 +1,37 @@
 package dad.micv.controller;
 
 import dad.micv.view.ContactoView;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
 
 public class ContactoController {
 
 	private ContactoView view;
-
+	private Stage addStage;
+	
 	public ContactoController() {
 
 		view = new ContactoView();
+
+		addStage = new Stage();
+		
+		
+		view.getTelefonoAddButton().setOnAction(e -> onTelefonoAddButton(e));
+		view.getEmailsAddButton().setOnAction(e -> onEmailsAddButton(e));
+		view.getWebsAddButton().setOnAction(e -> onWebsAddButton(e));
+
+	}
+
+	private void onWebsAddButton(ActionEvent e) {
+
+	}
+
+	private void onEmailsAddButton(ActionEvent e) {
+
+	}
+
+	private void onTelefonoAddButton(ActionEvent e) {
+
 	}
 
 	public ContactoView getView() {
